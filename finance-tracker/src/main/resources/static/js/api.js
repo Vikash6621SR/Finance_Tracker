3777e3caa0b7c8fee3b36d82154c181482d87e4b
Vikash6621SR <vikashsr412@gmail.com>
2026-08-17 16:51:35 +0530

    Update api.js

--- finance-tracker/src/main/resources/static/js/api.js
+++ finance-tracker/src/main/resources/static/js/api.js
@@ -119,6 +119,17 @@ const FinanceAPI = {
             );
         }
     },
+    
+
+    async me() {
+
+    return await apiRequest(
+        "/auth/me",
+        {
+            method: "GET"
+        }
+    );
+},
 
 
     /*
